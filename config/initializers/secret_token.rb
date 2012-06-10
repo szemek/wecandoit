bc778e07bfb3c80021d12d5b28226d8be4bf5d28
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2012-06-10 09:51:47 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webmuses::Application.config.secret_token = 'ebb68c50193820780f6c609677ba3996f2622812750a2804f5375433dfb6c7875c68d542176c3246086a2053cc19a54176e58d7a96546c9acfeffa2ce92afd42'
