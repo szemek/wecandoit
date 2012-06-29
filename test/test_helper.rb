bf31823f9ad0d08dc7483f043acd65ff21b35cef
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2012-06-29 20:26:10 +0200

    Skonfigurowany spork-testunit

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,27 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+require 'rubygems'
+require 'spork'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+  ENV["RAILS_ENV"] = "test"
+  require File.expand_path('../../config/environment', __FILE__)
+  require 'rails/test_help'
 
-  # Add more helper methods to be used by all tests here...
+  class ActiveSupport::TestCase
+    # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+    #
+    # Note: You'll currently still have to declare fixtures explicitly in integration tests
+    # -- they do not yet inherit this setting
+    fixtures :all
+
+    # Add more helper methods to be used by all tests here...
+  end
 end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
+end
+
