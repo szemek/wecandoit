19a85bc1662f8b95430bfdfe6ebe6a0bc5273e06
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2012-07-11 23:42:01 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -27,4 +27,14 @@ ActiveRecord::Schema.define(:version => 20120711170124) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+
 end
