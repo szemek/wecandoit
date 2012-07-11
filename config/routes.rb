b84bd3e96dc5f60fdb67cd6b5f053237c7690bd1
Monika <monikastarzyk@gmail.com>
2012-07-11 22:05:43 +0200

    M

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Webmuses::Application.routes.draw do
-  resources :onas 
+    root :to => 'home#index'
+    resources :onas
 
   get "home/index"
 
@@ -7,7 +8,7 @@ Webmuses::Application.routes.draw do
   # first created -> highest priority.
 
   # Sample of regular route:
-  match 'products/:onas' => 'onas#index'
+  #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
@@ -52,7 +53,7 @@ Webmuses::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'home#index'
+ 
 
   # See how all your routes lay out with "rake routes"
 
