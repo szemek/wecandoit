5a7aab061b0981c154062a1559d3bedd8e4c27d4
Monika <monikastarzyk@gmail.com>
2012-07-11 19:26:33 +0200

    M

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Webmuses::Application.routes.draw do
-  resources :onas
+  resources :onas 
 
   get "home/index"
 
@@ -7,7 +7,7 @@ Webmuses::Application.routes.draw do
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  match 'products/:onas' => 'onas#index'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
