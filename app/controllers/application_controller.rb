dd5d77b70b4bed75ddbddaba7ea6efaddd058a44
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2012-07-14 23:37:36 +0200

    turned off github authentication for a while

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate_user
+  #before_filter :authenticate_user
 
   def authenticate_user
     @current_user = User.find_by_id(session[:user_id])
