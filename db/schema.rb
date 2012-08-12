6780df75bcc29814402eaf8fb77a9594aa8a01ba
WojtekM <wojciech.mardyla@gmail.com>
2012-08-12 06:16:32 +0200

    Thin deleted from Gemfile dev/test group.

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(:version => 20120808201318) do
     t.string   "title"
   end
 
+  create_table "onas", :force => true do |t|
+    t.string   "name"
+    t.string   "info"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "questions", :force => true do |t|
     t.string   "title"
     t.text     "content"
