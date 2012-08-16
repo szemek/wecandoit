904bab1679dea7bfc9db47659f80bdd7d0827d9c
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2012-08-16 23:48:16 +0200

    renamed Webmuses to WeCanDoIt

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Webmuses::Application.configure do
+WeCanDoIt::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
