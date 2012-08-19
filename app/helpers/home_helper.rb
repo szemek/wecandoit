53b53c1c36fa583b1eb35e5a9f5ace7e9b2b76a2
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2012-08-19 21:11:13 +0200

    extracted logic from view to helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+  def last_link
+    link = Link.last
+    link_to(link.title, links_path) if link
+  end
+
+  def last_guide
+    guide = Guide.last
+    link_to(guide.title, guide) if guide
+  end
 end
