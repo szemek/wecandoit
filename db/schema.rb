3538a034ec02d19ca7d5a8e62e6ce2021477beec
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2012-08-31 22:12:40 +0200

    basic scaffold for projects page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120808201318) do
+ActiveRecord::Schema.define(:version => 20120831200958) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(:version => 20120808201318) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "title"
+  end
+
+  create_table "projects", :force => true do |t|
+    t.string   "user"
+    t.string   "repo"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "questions", :force => true do |t|
