24f905e3723b1b072d0a545620a68b7a43314587
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2012-09-01 10:51:46 +0200

    gravatar support

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     id = $(elem).attr('data-project')
 
     $.get('/projects/' + id + '/details', function(data){
+      gravatar = data['gravatar'].split('?')[0] + '?s=100&d=' + window.location.origin + '/assets/logos/octocat.png'
+      $(elem).find('img').attr('src', gravatar)
       $(elem).find('.description span').text(data['description'])
       $(elem).find('.last-commit-message span').text(data['last_commit_message'])
       $(elem).find('.last-commit-date span').text(data['last_commit_date'])
