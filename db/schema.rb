812ed89120653918b3534abcfb8cc7d83a3888ff
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2012-09-12 11:00:10 +0200

    added user_id to links

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120912084056) do
+ActiveRecord::Schema.define(:version => 20120912085904) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20120912084056) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "title"
+    t.integer  "user_id"
   end
 
   create_table "projects", :force => true do |t|
