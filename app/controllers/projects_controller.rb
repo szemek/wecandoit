a62faf7ca0a1ff1ddc7660ec808ca29b0f305863
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2012-09-15 09:39:31 +0200

    shuffle order of projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,7 @@ class ProjectsController < ApplicationController
   # GET /projects.json
   def index
     @projects = Project.all
+    @projects.shuffle!
 
     respond_to do |format|
       format.html # index.html.erb
