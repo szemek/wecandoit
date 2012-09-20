49bc6b2f95d6346228c1f3b4cf5db409c22ea176
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2012-09-20 16:30:08 +0200

    set up relationships between user and resources: links, guides, projects

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -2,4 +2,6 @@ class Guide < ActiveRecord::Base
   attr_accessible :content, :seo_url, :title
 
   validates :title, :content, :presence => true
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
 
   has_many :questions
   has_many :answers
+
+  has_many :projects
+  has_many :guides
+  has_many :links
 end
