83d58fa9b9eeea27d64124ac9ee8d5f330ee1efa
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2012-09-23 19:26:47 +0200

    skip Github authentication for development environment

--- app/controllers/oauth/github_controller.rb
+++ app/controllers/oauth/github_controller.rb
@@ -3,7 +3,13 @@ class Oauth::GithubController < ApplicationController
   skip_before_filter :authenticate_user
 
   def new
-    redirect_to Providers::Github.new(current_user).authorize_uri(request)
+    if Rails.env == "development"
+      user = User.find_or_create_by_username(:username => "test", :avatar_url => "/assets/logos/octocat.png")
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to Providers::Github.new(current_user).authorize_uri(request)
+    end
   end
 
   def create
