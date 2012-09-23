eb97a7c20ff688f58411cffda02b1e0e8729e618
Łukasz Młodzik <lukasz.mlodzik@gmail.com>
2012-09-23 12:25:18 +0200

    Configured friendly_id. If you have any data in guides table, please run this command in rails console: Guide.find_each(&:save)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120920203610) do
+ActiveRecord::Schema.define(:version => 20120923100304) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -21,14 +21,27 @@ ActiveRecord::Schema.define(:version => 20120920203610) do
     t.integer  "user_id"
   end
 
+  create_table "friendly_id_slugs", :force => true do |t|
+    t.string   "slug",                         :null => false
+    t.integer  "sluggable_id",                 :null => false
+    t.string   "sluggable_type", :limit => 40
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], :name => "index_friendly_id_slugs_on_slug_and_sluggable_type", :unique => true
+  add_index "friendly_id_slugs", ["sluggable_id"], :name => "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], :name => "index_friendly_id_slugs_on_sluggable_type"
+
   create_table "guides", :force => true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "seo_url"
+    t.string   "slug"
     t.integer  "user_id"
   end
+
+  add_index "guides", ["slug"], :name => "index_guides_on_slug", :unique => true
 
   create_table "links", :force => true do |t|
     t.string   "address"
