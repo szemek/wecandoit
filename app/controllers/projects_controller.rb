ee12e211d0426cedb4f13a6ea1832d6590fa634e
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2012-10-06 21:26:20 +0200

    buttons for editing & deleting projects (with fancy hover)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -74,7 +74,7 @@ class ProjectsController < ApplicationController
 
     respond_to do |format|
       if @project.update_attributes(params[:project])
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+        format.html { redirect_to projects_path, notice: 'Project was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
