ae9e24465154dc72323a4da8bdfa08e2a7a471a6
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2012-10-26 13:04:12 +0200

    order posts by created_at timestamp

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.reverse
+    @posts = Post.joins(:user).order('created_at DESC')
 
     @user = User.find(current_user.id).username
 
