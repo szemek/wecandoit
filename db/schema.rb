7e331ae2539243fec9454f5abed633f0bcf4b92d
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2012-11-07 21:57:06 +0100

    ActiveRecord: store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121026184628) do
+ActiveRecord::Schema.define(:version => 20121107203423) do
 
   create_table "friendly_id_slugs", :force => true do |t|
     t.string   "slug",                         :null => false
@@ -63,10 +63,11 @@ ActiveRecord::Schema.define(:version => 20121026184628) do
   create_table "projects", :force => true do |t|
     t.string   "username"
     t.string   "repo"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "user_id"
     t.string   "website"
+    t.text     "github_data"
   end
 
   create_table "users", :force => true do |t|
