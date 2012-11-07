0e1186aa7e39d13a7f354e7b46f8e5f120cb2d38
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2012-11-07 21:57:06 +0100

    ActiveRecord: store

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ActiveRecord::Base
   attr_accessible :repo, :username, :website
 
   belongs_to :user
+
+  store :github_data, accessors: [:gravatar, :description, :last_commit_date, :last_commit_message]
 end
