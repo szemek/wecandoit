9f49004d0080c8d0b70fa4db7f176d24c7de0ca0
Paweł Drożański <pawdro@gmail.com>
2012-11-07 19:34:07 +0100

    update redirect fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,7 +69,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.update_attributes(params[:post])
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to posts_path, notice: 'Post was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
