6fcce9b5c79fca750044e4a35db97a947cd7bd55
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2012-11-08 01:14:40 +0100

    github data refactoring

--- extras/github_data.rb
+++ extras/github_data.rb
@@ -2,42 +2,48 @@ require 'open-uri'
 require 'json'
 
 class GithubData
-  def initialize(user, repo)
-    @user = user
-    @repo = repo
+  def initialize(username, repository)
+    @username = username
+    @repository = repository
+
+    @gravatar = nil
+    @description = nil
+    @last_commit_message = nil
+    @last_commit_date = nil
   end
 
   def fetch!
-    @repo_info = repo_details
-    @commits_info = commits_details
+    begin
+      repository_details = JSON.parse(open(repository_url).read)
+      commits_details = JSON.parse(open(commits_url).read)
+
+      @gravatar = repository_details['owner']['avatar_url']
+      @description = repository_details['description']
+      @last_commit_message = commits_details.first['commit']['message']
+      date = commits_details.first['commit']['committer']['date']
+      @last_commit_date = DateTime.parse(date).in_time_zone('Warsaw')
+    rescue Exception => ex
+      puts "GithubData Exception: #{ex.message}"
+    end
   end
 
   def as_json(options = {})
     hash = {}
 
-    hash['gravatar'] = @repo_info['owner']['avatar_url']
-    hash['description'] = @repo_info['description']
-    hash['last_commit_message'] = @commits_info.first['commit']['message']
-    date = @commits_info.first['commit']['committer']['date']
-    hash['last_commit_date'] = DateTime.parse(date).in_time_zone('Warsaw')
+    hash['gravatar'] = @gravatar
+    hash['description'] = @description
+    hash['last_commit_message'] = @last_commit_message
+    hash['last_commit_date'] = @last_commit_date
 
     hash
   end
 
   private
-    def repo_details
-      JSON.parse(open(repo_url).read)
-    end
-
-    def commits_details
-      JSON.parse(open(commits_url).read)
-    end
-
-    def repo_url
-      "https://api.github.com/repos/#{@user}/#{@repo}"
+    def repository_url
+      "https://api.github.com/repos/#{@username}/#{@repository}"
     end
 
     def commits_url
-      "https://api.github.com/repos/#{@user}/#{@repo}/commits"
+      "https://api.github.com/repos/#{@username}/#{@repository}/commits"
     end
 end
