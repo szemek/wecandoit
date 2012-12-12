599f6cb57ffe5215941658ad2c4422a30d08d74c
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2012-12-12 13:29:45 +0100

    chat channel

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ WeCanDoIt::Application.routes.draw do
   get '/tree' => 'home#tree'
   get '/calendar' => 'home#calendar'
   get '/wall' => 'posts#index'
+  get '/chat/:channel' => 'chat#channel'
 
   get "/oauth/github" => 'oauth/github#new'
   get "/oauth/github/callback" => 'oauth/github#create'
