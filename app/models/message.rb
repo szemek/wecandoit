e25a63261f994d6d041a08bd9bee9e1f0c0cf403
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2012-12-16 11:18:11 +0100

    after_save callback for Pusher trigger

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,13 @@ class Message
   include Mongoid::Document
   field :channel, type: String
   field :content, type: String
+
+  after_save :push_message
+
+  private
+    def push_message
+      if Rails.env == 'production'
+        Pusher[self.channel].trigger('message', MessageDecorator.decorate(self).to_hash)
+      end
+    end
 end
